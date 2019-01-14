26deb16cac75511e744b6204c1f62e662860d9a5
Tristan Falcon <falcon.t830815@stu.SanJac.edu>
2019-01-14 11:21:24 -0600

    Updated source code documentation #time(30min)

--- src/scripts/vendors/gulpfile.js
+++ src/scripts/vendors/gulpfile.js
@@ -1,45 +1,94 @@
 
+/**
+ * This is the gulpfile.js, this file contains all gulp related plugins, tasks, and functions. 
+ * This file is used to automate tasks during development.
+ * 
+ * Structure:
+ *     > plugins
+ *     > file path variables
+ *     > functions
+ *     > tasks
+ * 
+ * @author Tristan Falcon 
+ **/
+
+/******************************************* gulp and gulp plugins ***************************************/
+// load Gulp
 const gulp = require( 'gulp' );
-const browserSync = require( 'browser-sync' ).create();
+
+// css related plugins 
 const sass = require( 'gulp-sass' );
-const uglify = require( 'gulp-uglify' );
-const rename = require( 'gulp-rename' );
 const autoprefixer = require( 'gulp-autoprefixer' );
-const sourcemaps = require( 'gulp-sourcemaps' );
-const browserify = require('browserify');
+
+// js related plugins 
+const uglify = require( 'gulp-uglify' );
 const babelify = require('babelify');
+const browserify = require('browserify');
 const source = require( 'vinyl-source-stream' );
 const buffer = require( 'vinyl-buffer' );
 
+// browser related plugins
+const browserSync = require( 'browser-sync' ).create();
 
+// utility plugins
+const rename = require( 'gulp-rename' );
+const sourcemaps = require( 'gulp-sourcemaps' );
+
+/******************************************* file paths ***************************************/
+// project root path, relative to gulpfile.js
 const root = '../../../';
 
-const buildFolder = root + '/build';
-
-const styleSRC = root + 'src/styles/main.scss';
-const styleDIST = root + 'build/styles/';
-const styleWatch = root + 'src/styles/**/*.scss';
-
-const scriptSRC = root + 'src/scripts/app.js';
-const scriptDIST = root + 'build/scripts/scripts';
-const scriptWatch = root + 'src/scripts/modules/*.js';
-
+// path to all.html pages in build directory 
 const htmlWatch = root + 'build/*.html';
 
+// path to build folder from project root
+const buildFolder = root + '/build';
+
+// path to main.scss in src directory 
+const styleSRC = root + 'src/styles/main.scss';
+// path to main.min.css in build directory 
+const styleDIST = root + 'build/styles/';
+// path to all .scss files in src directory
+const styleWatch = root + 'src/styles/**/*.scss';
+
+// path to app.js in src directory
+const scriptSRC = root + 'src/scripts/app.js';
+// path to app.min.js in build directory 
+const scriptDIST = root + 'build/scripts/scripts';
+// path to all .js files in src directory 
+const scriptWatch = root + 'src/scripts/modules/*.js';
+
+/******************************************* functions ***************************************/
+/**
+* This is the set browser_sync function used to start the local host 
+* @param done, passed to be used as a call back when function completes
+*/
 function browser_sync( done ) {
     browserSync.init({
         server: {
             baseDir: buildFolder
         }
     });
+    // call done function to return call back that function has completed running
     done();
 }
 
+/**
+* This is the reload function, used to update browser sync with current data
+* @param done, passed to be used as a call back when function completes
+*/
 function reload( done ) {
     browserSync.reload();
+    // call done function to return call back that function has completed running
     done();
 }
 
+/**
+* This is the css function, used to compile main.scss code into css,
+* minify the file, change the file name to include .min suffix,
+* and export the content into main.min.cs under the build directory. 
+* Changes are then injected into the browser to represent the style changes. 
+*/
 function css() {
     return gulp.src( styleSRC )
         .pipe( sourcemaps.init() )
@@ -57,6 +106,14 @@ function css() {
         .pipe( browserSync.stream() );
 };
 
+
+/**
+ * this is the js function, this function takes the app.js file and compiles 
+ * it into plain java script that can be utilized across different/older browsers. 
+ * The file is then minified, renamed to include the .min.js extension, and exported 
+ * to the app.min.js file under the build directory. 
+ * 
+ */
 function js() {
     return browserify({entries: [scriptSRC]})
         .transform( babelify, {presets: ['env']} )
@@ -71,14 +128,23 @@ function js() {
         .pipe( browserSync.stream() );
 };
 
+/**
+ * This is the watch_files function, this function tells gulp what files to watch 
+ * and check for changes. 
+ */
 function watch_files() {
     gulp.watch( styleWatch, gulp.series(css));
     gulp.watch( scriptWatch, gulp.series(js));
     gulp.watch( htmlWatch, reload)
 }
 
+/******************************************* tasks ***************************************/
+// calls css function 
 gulp.task("css", css);
+// calls js function
 gulp.task("js", js);
 
+// watch task, initializes browser sync and begins watching files
 gulp.task( 'watch', gulp.parallel(browser_sync, watch_files));
+// default task, updates css, js, and html in browser sync
 gulp.task( 'default', gulp.parallel(css, js));
